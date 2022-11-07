9ade3f019dc334be92ea41f208588b3a8d8f84fd
Davi Castro <davialmeida47jd@gmail.com>
2022-11-07 01:15:29 -0300

    feat(backend): created views to summary and hosts-csv and registed to url

--- app_manager/views.py
+++ app_manager/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from vuln_manager.custom_methods import IsAuthenticatedCustom
 from vuln_manager.utils import CustomPagination, get_query
 from django.db.models import Count
+from users.models import CustomUser
+import csv
+import codecs
 
 class HostView(ModelViewSet):
     queryset = Hosts.objects.select_related('group', 'created_by')
@@ -68,3 +71,62 @@ class HostGroupView(ModelViewSet):
     def create(self, request, *args, **kwargs):
         request.data.update({'created_by_id': request.user.id})
         return super().create(request, *args, **kwargs)
+
+
+class SummaryView(ModelViewSet):
+    http_method_names = ('get',)
+    permission_classes = (IsAuthenticatedCustom,)
+    queryset = HostView.queryset
+
+    def list(self, request, *args, **kwargs):
+        total_host = HostView.queryset.filter(
+            remaining__gt=0
+        ).count()
+        total_group = HostGroupView.queryset.count()
+        total_users = CustomUser.objects.filter(is_superuser=False).count()
+
+        return Response({
+            'total_host': total_host,
+            'total_group': total_group,
+            'total_users': total_users
+        })
+
+
+class HostCSVLoaderView(ModelViewSet):
+    http_method_names = ('post')
+    queryset = HostView.queryset
+    permission_classes = (IsAuthenticatedCustom,)
+    serializer_class = HostSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            data = request.FILES['data']
+        except Exception as e:
+            raise Exception('You need to upload a file CSV data')
+        
+        host_items = []
+
+        try:
+            csv_reader = csv.reader(codecs.iterdecode(data, 'utf-8'))
+            for row in csv_reader:
+                if not row[0]:
+                    continue
+                host_items.append(
+                    {
+                        'group_id': row[0],
+                        'total': row[1],
+                        'name': row[2],
+                        'added_by_id': request.user.id
+                    }
+                )
+        except csv.Error as e:
+            raise Exception('File CSV not valid, error: {}'.format(e))
+        
+        if not host_items:
+            raise Exception('File CSV is empty')
+        
+        data_validation = self.serializer_class(data=host_items, many=True)
+        data_validation.is_valid(raise_exception=True)
+        data_validation.save()
+
+        return Response({'success': 'Host itens has been added'})
