3054305acd770cea6815ed9f56f97b729c56ffda
Davi Castro <davialmeida47jd@gmail.com>
2022-11-06 22:17:52 -0300

    feat(backend): registered userActivities and users view to url and routes

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, UserActivities
 
 
 admin.site.register(CustomUser)
+admin.site.register(UserActivities)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (
-    CreateUserView, LoginView, UpdatePasswordView, CustomUserView
+    CreateUserView, LoginView, UpdatePasswordView, CustomUserView, 
+    UserActivitiesView, UsersView
 )
 
 from rest_framework.routers import DefaultRouter
@@ -11,6 +12,8 @@ router.register('create-user', CreateUserView, 'create user')
 router.register('login', LoginView, 'login')
 router.register('update-password', UpdatePasswordView, 'update password')
 router.register('me', CustomUserView, 'me')
+router.register('activities', UserActivitiesView, 'activities')
+router.register('users', UsersView, 'users')
 
 urlpatterns = [
     path('', include(router.urls)),
