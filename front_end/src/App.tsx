74c330ae3d26fbcfbdfa2db3a89c619d62f2000f
Davi Castro <davialmeida47jd@gmail.com>
2022-11-08 00:53:02 -0300

    feat(front-end): created basic form for login and imported antd to app

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react'
 import Router from './router'
+import 'antd/dist/antd.min.css'
 
 const App:FC = () => {
     return <Router />
--- front_end/src/pages/Login.tsx
+++ front_end/src/pages/Login.tsx
@@ -1,7 +1,28 @@
 import {FC} from 'react'
+import {Form, Input, Button} from 'antd'
 
 const Login:FC = () => {
-    return <h1>Login</h1>
+    return <div className='login'>
+        <div className="inner">
+            <div className="header">
+                <h3>Sign In</h3>
+                <h2>Vuln Manager</h2>
+            </div>
+            <Form
+                layout={'vertical'}
+            >
+                <Form.Item label="Field A">
+                    <Input placeholder="input placeholder" />
+                </Form.Item>
+                <Form.Item label="Field B">
+                    <Input placeholder="input placeholder" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary">Submit</Button>
+                </Form.Item>
+            </Form>
+        </div>
+    </div>
 }
 
 export default Login
