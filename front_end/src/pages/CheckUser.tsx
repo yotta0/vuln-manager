8fb616dfb348515fb85e90b5b941c934501be3c5
Davi Castro <davialmeida47jd@gmail.com>
2022-11-08 03:29:21 -0300

    feat(front-end): added on submit axios request to login in back-end

--- front_end/src/pages/CheckUser.tsx
+++ front_end/src/pages/CheckUser.tsx
@@ -1,13 +1,37 @@
-import {FC} from 'react'
+import { notification } from 'antd'
+import axios from 'axios'
+import {FC, useState} from 'react'
 import AuthComponent from '../components/AuthComponent'
+import { LoginUrl } from '../utils/network'
+import { CustomAxiosError, DataProps } from '../utils/types'
 
 const CheckUser:FC = () => {
+
+    const [loading, setLoading] = useState(false)
+
+    const onSubmit = async (values: DataProps) => {
+        setLoading(true)
+        const response = await axios.post(LoginUrl, {...values, is_new_user: true}).catch(
+           (e: CustomAxiosError) => {
+               notification.error({
+                    message: 'User Check Error',
+                    description: e.response?.data.error
+               })
+           }
+        )
+        if(response){
+            console.log('Check completed')
+        }
+        setLoading(false)
+    }
+
     return <AuthComponent 
         titleText='Verify Yourself'
         bottonText='Submit'
         linkText='Already have an account?'
         isPassword={false}
         linkPath='/login'
+        onSubmit={onSubmit}
     />
 }
 
--- front_end/src/pages/Login.tsx
+++ front_end/src/pages/Login.tsx
@@ -1,8 +1,32 @@
-import {FC} from 'react'
+import {FC, useState} from 'react'
 import AuthComponent from '../components/AuthComponent'
+import { CustomAxiosError, DataProps } from '../utils/types'
+import axios from 'axios'
+import { LoginUrl } from '../utils/network'
+import { notification } from 'antd'
+
 
 const Login:FC = () => {
-    return <AuthComponent />
+
+    const [loading, setLoading] = useState(false)
+
+    const onSubmit = async (values: DataProps) => {
+        setLoading(true)
+        const response = await axios.post(LoginUrl, values).catch(
+           (e: CustomAxiosError) => {
+               notification.error({
+                    message: 'Login Error',
+                    description: e.response?.data.error
+               })
+           }
+        )
+        if(response){
+            console.log(response)
+        }
+        setLoading(false)
+    }
+
+    return <AuthComponent onSubmit={onSubmit} loading={loading}/>
 }
 
 export default Login
