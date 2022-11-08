ef21b732917417353ba0ab6af7db39b358ac477d
Davi Castro <davialmeida47jd@gmail.com>
2022-11-08 01:52:51 -0300

    feat(front-end): created component props to handle AuthComponent

--- front_end/src/components/AuthComponent.tsx
+++ front_end/src/components/AuthComponent.tsx
@@ -1,24 +1,40 @@
 import {FC} from 'react'
 import {Form, Input, Button} from 'antd'
+import {Link} from 'react-router-dom'
 
-const AuthComponent:FC = () => {
+interface AuthComponentProps {
+    titleText?: string,
+    isPassword?: boolean,
+    bottonText?: string,
+    linkText?: string,
+    linkPath?: string
+}
+
+const AuthComponent:FC<AuthComponentProps> = ({
+    titleText = 'Sign In',
+    isPassword = true,
+    bottonText = 'Login',
+    linkText = 'New User',
+    linkPath = '/check-user'
+}) => {
     return <div className='login'>
         <div className="inner">
             <div className="header">
-                <h3>Sign In</h3>
+                <h3>{titleText}</h3>
                 <h2>Vuln Manager</h2>
             </div>
             <Form layout={'vertical'}>
                 <Form.Item label="Email">
                     <Input placeholder="input placeholder" type='email'/>
                 </Form.Item>
-                <Form.Item label="Password">
+                {isPassword && <Form.Item label="Password">
                     <Input placeholder="input placeholder" type='password'/>
-                </Form.Item>
+                </Form.Item>}
                 <Form.Item>
-                    <Button type="primary" block>Submit</Button>
+                    <Button type="primary" block>{bottonText}</Button>
                 </Form.Item>
             </Form>
+            <Link to={linkPath} >{linkText}</Link>
         </div>
     </div>
 }
