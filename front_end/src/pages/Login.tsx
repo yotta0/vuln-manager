f9f2bde7fc43298c95d2741c1dead6c4c0cc1328
Davi Castro <davialmeida47jd@gmail.com>
2022-11-09 00:24:28 -0300

    feat(front-end): created login props to handle logins and added 'token' to local storage

--- front_end/src/pages/Login.tsx
+++ front_end/src/pages/Login.tsx
@@ -1,27 +1,37 @@
 import {FC, useState} from 'react'
 import AuthComponent from '../components/AuthComponent'
 import { CustomAxiosError, DataProps } from '../utils/types'
+import { tokenName } from '../utils/data'
 import axios from 'axios'
 import { LoginUrl } from '../utils/network'
 import { notification } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
+interface LoginDataProps {
+    data: {
+        access: string
+    }
+}
 
 const Login:FC = () => {
 
     const [loading, setLoading] = useState(false)
 
+    const history = useNavigate()
+
     const onSubmit = async (values: DataProps) => {
         setLoading(true)
-        const response = await axios.post(LoginUrl, values).catch(
+        const response: LoginDataProps = await axios.post(LoginUrl, values).catch(
            (e: CustomAxiosError) => {
                notification.error({
                     message: 'Login Error',
                     description: e.response?.data.error
                })
            }
-        )
+        ) as LoginDataProps
         if(response){
-            console.log(response)
+            localStorage.setItem(tokenName, response.data.access)
+            history('/')
         }
         setLoading(false)
     }
